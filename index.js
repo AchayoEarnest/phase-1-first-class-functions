6a0eef9452d078d69dcac8c924e36c743cecd0d6
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-09 11:12:42 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        return;
+    };
+}
+
+function returnsAnAnonymousFunction(){
+    return  () => {}
+}
